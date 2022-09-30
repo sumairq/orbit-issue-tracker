d3299113fb58611fe1ddf1d49141de9d3d31402c
sumairq <sumairqaisar992@gmail.com>
2022-09-30 12:38:00 +0500

    Implement controllers, database seed scripts, and entities

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,53 @@
-import express from 'express'
-import cors from 'cors'
-
+import 'module-alias/register';
 import 'dotenv/config';
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
 
+import createDatabaseConnection from 'database/connection';
+import { authenticateUser } from 'middleware/authentication';
+import projectsRoutes from 'controllers/projects';
+import issuesRoutes from 'controllers/issues';
+import { RouteNotFoundError } from 'errors';
+import { errorHandler } from 'errors/errorHandler';
 
+const establishDatabaseConnection = async (): Promise<void> => {
+  try {
+    await createDatabaseConnection();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const initializeExpress = () => {
-    const app = express();
-  
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.listen(process.env.PORT || 3000);
-  };
+const initializeExpress = (): void => {
+  const app = express();
+  const PORT = 3000;
 
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded());
 
+  app.use((_req, res, next) => {
+    res.respond = (data): void => {
+      res.status(200).send({ data });
+    };
+    next();
+  });
 
-const initializeApp = async () => {
-    initializeExpress();
-  };
+  app.use('/', authenticateUser);
 
-  initializeApp()
+  app.use('/', projectsRoutes);
+  app.use('/', issuesRoutes);
+
+  app.use((req, _res, next) => next(new RouteNotFoundError(req.originalUrl)));
+  app.use(errorHandler);
+
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+};
+
+const initializeApp = async (): Promise<void> => {
+  await establishDatabaseConnection();
+  initializeExpress();
+};
+
+initializeApp();
