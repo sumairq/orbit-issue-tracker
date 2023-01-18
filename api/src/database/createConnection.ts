e0604ed0a5c11ec55804a79bec04c7efe8f0759e
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-01-18 13:32:20 +0500

    Fix certificate issue

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -1,4 +1,6 @@
 import { DataSource } from 'typeorm';
+import path from 'path';
+import * as fs from 'fs';
 
 import * as entities from 'entities';
 
@@ -11,6 +13,10 @@ const createDatabaseConnection = (): Promise<DataSource> =>
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: Object.values(entities),
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync(path.join(__dirname, '../../ca-certificate.crt')),
+    },
     synchronize: true,
   }).initialize();
 
--- client/webpack.config.production.js
+++ client/webpack.config.production.js
@@ -61,7 +61,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
-        API_URL: JSON.stringify('https://jira-api.sumair.dev'),
+        API_URL: JSON.stringify('http://api.sumair.ml:3000'),
       },
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
