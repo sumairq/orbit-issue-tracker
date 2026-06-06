897cde024fe61dab60fa3ad17d625cb8c064dbf5
sumairq <sumairqaisar992@gmail.com>
2026-06-07 02:50:50 +0500

    Add cors settings

--- api/src/index.ts
+++ api/src/index.ts
@@ -28,7 +28,11 @@ const ensureDatabaseConnection = (): Promise<unknown> => {
 const createApp = (): Express => {
   const app = express();
 
-  app.use(cors());
+  app.use(cors({
+    origin: 'https://orbitclient-sepia.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
   app.use(express.json());
   app.use(express.urlencoded());
 
